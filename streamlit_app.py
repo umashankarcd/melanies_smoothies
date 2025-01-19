862ebde9bc1bd5d7f90125b84d8078f0951bf616
Umashankar Dyavalingaiah <72979033+umashankarcd@users.noreply.github.com>
2025-01-19 00:19:21 +0000

    Update streamlit_app.py
    Added LOC functiojn

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,11 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
+#convert the snowpark dataframe to a Pandas dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe,max_selections=5)
 
 if ingredients_list:
